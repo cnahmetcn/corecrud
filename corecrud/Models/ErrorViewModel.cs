3d02d5ad4ae5d6bd3f3f7f37d9f5645b8e549dc2
Ahmet CAN <cnahmet@outlook.com>
2021-01-28 13:44:18 +0300

    upload

---
+++ corecrud/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace corecrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
