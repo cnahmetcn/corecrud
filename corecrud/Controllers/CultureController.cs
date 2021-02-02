a2e51d503514a8365c92810c7974cd0d46895520
Ahmet CAN <cnahmet@outlook.com>
2021-02-02 23:03:05 +0300

    add lang

---
+++ corecrud/Controllers/CultureController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace corecrud.Controllers
+{
+    public class CultureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CultureManagement(string culture, string returnUrl)
+        {
+            string url = returnUrl;
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.Now.AddDays(1) });
+            // return RedirectToAction(nameof(Index));
+            if (returnUrl != "~/")
+            {
+                url = returnUrl.Replace(returnUrl.Substring(2, 2), culture);
+            }
+            return LocalRedirect(url);
+        }
+    }
+}
--- corecrud/Startup.cs
+++ corecrud/Startup.cs
@@ -2,12 +2,16 @@ using corecrud.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,9 +29,30 @@ namespace corecrud
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            
             services.AddDbContext<UsersContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("UsersContext")));
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            //services.AddSingleton<LocalizationService>();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddMvc()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                .AddDataAnnotationsLocalization();
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("en"),
+                    new CultureInfo("tr")
+                };
+                options.DefaultRequestCulture = new RequestCulture("en");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,13 +73,25 @@ namespace corecrud
 
             app.UseRouting();
 
+
+            var requestlocalizationOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(requestlocalizationOptions.Value);
             app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //});
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    name: "Default",
+                    pattern: "{culture}/{controller}/{action}/{id?}",
+                    defaults: new { culture = "en", controller = "Home", action = "Index" }
+                    );
             });
         }
     }
